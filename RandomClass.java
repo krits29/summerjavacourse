560abf86d22f05b0094bd04bb5c7897b3c8499d9
krits29 <31025768+krits29@users.noreply.github.com>
2019-07-19 11:58:42 -0700

    Using the Random Class

--- RandomClass.java
+++ RandomClass.java
@@ -6,13 +6,29 @@ public class RandomClass
 	{
 		Random randy = new Random();
 		
-		System.out.println( randy.nextInt() );
+		System.out.println((int)Math.random() * 100); //we want a number between 0 and 100. below we will cast it	
+		System.out.println((int)Math.random() * 100);
+		System.out.println((int)Math.random() * 100);
+		System.out.println((int)Math.random() * 100);
+		System.out.println((int)Math.random() * 100);
+		System.out.println((int)Math.random() * 100);
+		System.out.println((int)Math.random() * 100);
+		System.out.println((int)Math.random() * 100);
+		
+		/* System.out.println( randy.nextInt( 21 ) + 30 );
+		System.out.println( randy.nextInt( 21 ) + 30 );
+		System.out.println( randy.nextInt( 21 ) + 30 );
+		System.out.println( randy.nextInt( 21 ) + 30 );
+		System.out.println( randy.nextInt( 21 ) + 30 ); */
+		
+		
+		/* System.out.println( randy.nextInt() );
 		System.out.println( randy.nextInt() );
 		System.out.println( randy.nextInt() );
 
 		System.out.println( randy.nextInt( 4 ) );
 		System.out.println( randy.nextInt( 4 ) );
 		System.out.println( randy.nextInt( 4 ) );
-		System.out.println( randy.nextInt( 4 ) );
+		System.out.println( randy.nextInt( 4 ) ); */
 	}
 }
